9c2d1569d580f62bf2fe9aae7705390dfd5dd577
Cardinal, Maxime | Maxime | ECBD <maxime.cardinal@rakuten.com>
2018-05-01 22:10:23 +0900

    Use strictEntity to bypass the stream drought

--- src/main/java/com/lightbend/akka/http/sample/KokoProxyServer.java
+++ src/main/java/com/lightbend/akka/http/sample/KokoProxyServer.java
@@ -6,22 +6,23 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.*;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.pattern.PatternsCS;
 import akka.stream.ActorMaterializer;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
 
 import akka.stream.javadsl.Flow;
+import akka.util.ByteString;
 import akka.util.Timeout;
 import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
 
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class KokoProxyServer extends AllDirectives {
-
 
     Timeout timeout = new Timeout(Duration.create(30, TimeUnit.SECONDS)); // usually we'd obtain the timeout from the system's configuration
 
@@ -38,7 +39,7 @@ public class KokoProxyServer extends AllDirectives {
         ActorRef directContentRequestActor = system.actorOf(DirectRequestActor.props(), "directRequestActor");
         ActorRef requestCacheActor = system.actorOf(ProxyCacheActor.props(), "proxyCacheActor");
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute(directContentRequestActor, requestCacheActor).flow(system, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute(directContentRequestActor, requestCacheActor,materializer).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 3128), materializer);
         System.out.println("Server online at http://localhost:3128/");
     }
@@ -47,19 +48,31 @@ public class KokoProxyServer extends AllDirectives {
         PatternsCS.ask(requestCacheActor, new ProxyMessages.WriteCache(request,response), timeout);
     }
 
-    public Route createRoute(ActorRef directContentRequestActor, ActorRef requestCacheActor) {
+    public Route createRoute(ActorRef directContentRequestActor, ActorRef requestCacheActor,ActorMaterializer materializer) {
 
         final Route extractRequest = extractRequest(request -> {
                     CompletionStage<HttpResponse> futureHttpResponse = PatternsCS.ask(directContentRequestActor, request.getUri().toString(), timeout).
                             thenApply(obj -> (HttpResponse) obj);
                     return onSuccess(() -> futureHttpResponse, httpResponse ->
                             {
+                                final CompletionStage<HttpEntity.Strict> strictEntity = httpResponse.entity()
+                                        .toStrict(FiniteDuration.create(3, TimeUnit.SECONDS).toMillis(), materializer);
+
+                                try {
+                                    ByteString content = strictEntity.toCompletableFuture().get().getData();
+                                    ContentType contentType = strictEntity.toCompletableFuture().get().getContentType();
+                                    return complete(HttpResponse.create().withEntity(contentType,content).withStatus(httpResponse.status()).withHeaders(httpResponse.getHeaders()));
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                } catch (ExecutionException e) {
+                                    e.printStackTrace();
+                                }
+
                                 //TODO the httpResponse.entity is a stream so the second time it gets called (by either create cache or complete) it crashes
                                 //Need to find a way to avoid this.. maybe toStrict might fix it ..
                                 // or always return the cache after it was created..but I wanted to avoid to wait for the cache to be created to send the response..
-                                //createCache(requestCacheActor, request, httpResponse);
-                                //return complete("Dummy response");
-                                return complete(httpResponse.entity());
+                                //createCache(requestCacheActor, request, httpResponse1);
+                                return     complete(StatusCodes.INTERNAL_SERVER_ERROR);
                             }
                     );
                 }
